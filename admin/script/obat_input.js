9b627a6db56975eecaf68f837cdd57b3a857bfe6
zaky10062001 <zakytjr5@gmail.com>
2023-12-05 08:50:08 +0300

    26.Input Obat

---
+++ admin/script/obat_input.js
@@ -0,0 +1,17 @@
+$('#input').submit(function(e){
+    
+    e.preventDefault();
+
+    var formData = new FormData(this);
+
+    $.ajax({
+        type: 'POST',
+        url: host+"/obat_input.php",
+        data: formData,
+        cache: false, contentType: false, processData: false, dataType: 'json',
+        success: (result) => {
+            console.log(result);
+            alert("Data Berhasil di Tambah")
+        },
+    });
+});
