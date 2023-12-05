c621fcffad428ce70e6c7df47bf5a3768f07ee76
zaky10062001 <zakytjr5@gmail.com>
2023-12-05 10:39:12 +0300

    26.Input Obat

--- admin/obat_input.php
+++ admin/obat_input.php
@@ -45,6 +45,7 @@
         <!--end dashboard-->
         <!-- Optional JavaScript -->
         <script type="text/JavaScript" src="assets/lib/bootstrap-4.6.2-dist/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script type="text/JavaScript" src="assets/js/obat_input.js"></script>
     </body>
 </html>
--- admin/script/obat_input.js
+++ admin/script/obat_input.js
@@ -3,15 +3,22 @@ $('#input').submit(function(e){
     e.preventDefault();
 
     var formData = new FormData(this);
-
     $.ajax({
-        type: 'POST',
         url: host+"/obat_input.php",
+        type: 'POST',
         data: formData,
-        cache: false, contentType: false, processData: false, dataType: 'json',
-        success: (result) => {
-            console.log(result);
-            alert("Data Berhasil di Tambah")
+        async: false,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            if (response.status == 200) {
+                alert(response.msg);
+                console.log(response.body.data);
+            } else {
+                alert('Gagal menyimpan data.');
+            }
         },
     });
 });
+
