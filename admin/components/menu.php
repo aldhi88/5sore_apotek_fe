27fa2a4a223c198bb8f253c760294640c39776f0
Aldhi <aldhi.albadri@gmail.com>
2023-12-05 08:29:33 +0700

    fix

--- admin/components/menu.php
+++ admin/components/menu.php
@@ -32,6 +32,28 @@
         </ul>
 
         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Supplier</span>
+            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+                <span data-feather="plus-circle"></span>
+            </a>
+        </h6>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link py-1" href="#">
+                    <i class="fa-solid fa-circle-dot fa-fw"></i>
+                    Data Supplier
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link py-1" href="#">
+                    <i class="fa-solid fa-circle-dot fa-fw"></i>
+                    Supplier Baru
+                </a>
+            </li>
+            
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
             <span>Obat</span>
             <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
                 <span data-feather="plus-circle"></span>
