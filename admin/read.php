5e1da62f8d9d9997931d49b7de97d30668204972
zaky10062001 <zakytjr5@gmail.com>
2023-12-08 13:13:52 +0300

    21.Edit Kategori

---
+++ admin/read.php
@@ -0,0 +1,27 @@
+<?php
+// $conn = mysqli_connect('localhost', 'root', '', 'showroom');
+$response = [
+    'status' => 200,
+    'msg' => 'Data berhasil diinsert',
+    'body' => [
+        'data' => [
+            'kode' => 1,
+            'nama' => 'Panadol',
+            'kode_kategori' => 1,
+            'kode_supplier' => 2,
+            'gambar' => '1.jpg',
+            'harga' => 20000,
+        ]
+    ]
+];
+
+
+// $result = mysqli_query($conn, "SELECT * FROM tb_merek ORDER BY id_merek ASC");
+// while ($row = mysqli_fetch_assoc($result)) {
+//     $data[] = $row;
+// }
+
+// $response['body']['data'] = $data;
+echo json_encode($response);
+
+
--- admin/script/update_kategori.js
+++ admin/script/update_kategori.js
@@ -1,29 +1,35 @@
 $('.btn-edit').on('click', function () {
     var categoryId = $(this).data('id');
     
+    // Panggil API untuk membaca data berdasarkan ID
     $.ajax({
-        url: host+"update_kategori.php",
-        type: 'GET',
+        url: host + "read_kategori_by_id.php?id=" + categoryId,
         success: function (data) {
+            // Parsing data dan mengisi formulir edit
             $('#kode').val(data.kode);
             $('#nama').val(data.nama);
             $('#formKategori').attr('data-id', categoryId);
         },
     });
 });
+
 $('#formKategori').submit(function(e){
     e.preventDefault();
-    var formData = new FormData(this);
+    
+    // Ambil ID dari formulir
+    var categoryId = $(this).data('id');
+    
+    // Panggil API untuk pembaruan
     $.ajax({
         type: 'POST',
-        url: host+"update_kategori.php",
-        data: formData,
+        url: host + "update_kategori.php?id=" + categoryId,
+        data: new FormData(this),
         cache: false,
         contentType: false, 
         processData: false, 
         dataType: 'json',
-        success: (result) => {
+        success: function (result) {
             alert(result.msg);
         },
     });
-})
+});
