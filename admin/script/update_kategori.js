06d23e4b630354de0c3c79e773d9bb295728117f
zaky10062001 <zakytjr5@gmail.com>
2023-12-08 10:53:18 +0300

    21.Edit Kategori

---
+++ admin/script/update_kategori.js
@@ -0,0 +1,29 @@
+$('.btn-edit').on('click', function () {
+    var categoryId = $(this).data('id');
+    
+    $.ajax({
+        url: host+"update_kategori.php",
+        type: 'GET',
+        success: function (data) {
+            $('#kode').val(data.kode);
+            $('#nama').val(data.nama);
+            $('#formKategori').attr('data-id', categoryId);
+        },
+    });
+});
+$('#formKategori').submit(function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    $.ajax({
+        type: 'POST',
+        url: host+"update_kategori.php",
+        data: formData,
+        cache: false,
+        contentType: false, 
+        processData: false, 
+        dataType: 'json',
+        success: (result) => {
+            alert(result.msg);
+        },
+    });
+})
---
+++ admin/update_kategori.php
@@ -0,0 +1,20 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Update Kategori</h1>
+</div>
+
+<div class="row">
+    <div class="col">
+        <form id="formKategori">
+            <div class="form-group">
+                <label for="kode">Kode Kategori</label>
+                <input type="text" class="form-control" id="kode" name="kode" />
+            </div>
+            <div class="form-group">
+                <label for="nama">Nama Kategori</label>
+                <input type="text" class="form-control" id="nama" name="nama" />
+            </div>
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>
