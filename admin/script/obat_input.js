194ba5ba2976df6c474e3d33efd8b91f5487a4aa
zaky10062001 <zakytjr5@gmail.com>
2023-12-19 07:45:38 +0300

    56.Bugfix Select Option Obat Input

--- admin/script/obat_input.js
+++ admin/script/obat_input.js
@@ -13,4 +13,36 @@ $('#input').submit(function(e){
             alert(result.msg);
         },
     });
-})
+  })
+document.addEventListener('DOMContentLoaded', function() {
+  const url = host+'read_supplier.php'; // API URL
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onload = function() {
+     if (this.status === 200) {
+       const supplier = JSON.parse(this.responseText);
+       let options = '<option value="">--Pilih Supplier--</option>';
+       supplier.forEach(function(supplier) {
+         options += `<option value="${supplier.kode_supplier}">${supplier.nama}</option>`;
+       });
+       document.getElementById('kode_supplier').innerHTML = options;
+     }
+  };
+  xhr.send();
+ 
+  // AJAX request for kategori
+  const url2 = host+'read_kategori.php'; // API URL
+  const xhr2 = new XMLHttpRequest();
+  xhr2.open('GET', url2, true);
+  xhr2.onload = function() {
+     if (this.status === 200) {
+       const kategori = JSON.parse(this.responseText);
+       let options = '<option value="">--Pilih Kategori--</option>';
+       kategori.forEach(function(kategori) {
+         options += `<option value="${kategori.kode_kategori}">${kategori.nama}</option>`;
+       });
+       document.getElementById('kode_kategori').innerHTML = options;
+     }
+  };
+  xhr2.send();
+ });
