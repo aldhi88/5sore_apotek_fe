7ab9de21df2c98eca0df845500b11aa29fdaf7ac
zaky10062001 <zakytjr5@gmail.com>
2023-12-22 18:00:11 +0300

    perbaikan

--- admin/script/obat_input.js
+++ admin/script/obat_input.js
@@ -14,34 +14,58 @@ $('#input').submit(function(e){
         },
     });
   })
-  document.addEventListener('DOMContentLoaded', function () {
-    fetchSupplier();
-    fetchKategori();
-   });   
-   function fetchSupplier() {
-    fetch(host+'read_supplier.php')
-       .then(response => response.json())
-       .then(data => {
-         let select = document.getElementById('kode_supplier');
-         data.body.data.forEach(item => {
-           let option = document.createElement('option');
-           option.value = item.kode_supplier;
-           option.text = item.kode;
-           select.add(option);
-         });
-       });
-   }
-   
-   function fetchKategori() {
-    fetch(host+'read_kategori.php')
-       .then(response => response.json())
-       .then(data => {
-         let select = document.getElementById('kode_kategori');
-         data.body.data.forEach(item => {
-           let option = document.createElement('option');
-           option.value = item.kode_kategori;
-           option.text = item.kode;
-           select.add(option);
-         });
-       });
-   }
+
+   function populasiKategori() {
+    $.ajax({
+        type: "GET",
+        url: host+"read_kategori.php", // Gantilah dengan URL API kategori
+        dataType: "json",
+        async: true,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            var categories = data.body.data;
+            var select = $("#kode_kategori");
+
+            // Kosongkan opsi pemilihan sebelum mengisi kembali
+            select.empty();
+
+            // Tambahkan opsi pemilihan untuk setiap kategori
+            for (var i = 0; i < categories.length; i++) {
+                select.append(`<option value="${categories[i].kode}">${categories[i].kode}</option>`);
+            }
+        },
+    });
+}
+// Panggil fungsi untuk mengisi opsi pemilihan kategori saat halaman dimuat
+$(document).ready(function () {
+    populasiKategori();
+});
+function populasiSuplier() {
+  $.ajax({
+      type: "GET",
+      url: host+"read_supplier.php", // Gantilah dengan URL API kategori
+      dataType: "json",
+      async: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function (data) {
+          var categories = data.body.data;
+          var select = $("#kode_supplier");
+
+          // Kosongkan opsi pemilihan sebelum mengisi kembali
+          select.empty();
+
+          // Tambahkan opsi pemilihan untuk setiap kategori
+          for (var i = 0; i < categories.length; i++) {
+              select.append(`<option value="${categories[i].kode}">${categories[i].kode}</option>`);
+          }
+      },
+  });
+}
+// Panggil fungsi untuk mengisi opsi pemilihan kategori saat halaman dimuat
+$(document).ready(function () {
+  populasiSuplier();
+});
