ab1953b8e1800ed7e79788df4cee09e56ed2a2b2
zaky10062001 <zakytjr5@gmail.com>
2023-12-25 12:27:55 +0300

    push ulang

--- admin/obat_input.php
+++ admin/obat_input.php
@@ -1,40 +1,41 @@
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Input Obat</h1>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3">
+    <h6><a href="?page=obat_data">Kembali</a></h6>
 </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Edit Obat</h1>
+</div>
+
 <div class="row">
-    <div class="col-4">
-      <form id="input" action="#" method="POST" enctype="multipart/form-data">
-        <div class="form-group">
-          <label for="gambar">Masukkan Gambar</label> <br>
-          <input id="gambar" name="gambar" type="file" class="form-control-file" accept="image/*" />
-        </div>
-        <div class="form-group">
-          <label for="kode">Kode Obat</label>
-          <input type="text" class="form-control" id="kode" name="kode" placeholder="Masukkan kode obat" />
-        </div>
-        <div class="form-group">
-          <label for="nama">Nama Obat</label>
-          <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan nama obat" />
-        </div>
-        <div class="form-group">
-          <label for="kode_supplier">Supplier </label>
-          <select class="form-control" id="kode_supplier" name="kode_supplier">
-            <!-- Opsi supplier akan dimasukkan melalui Ajax -->
-          </select>
-        </div>
-                <div class="form-group">
-          <label for="kode_kategori">Kategori </label>
-          <select class="form-control" id="kode_kategori" name="kode_kategori">
-            <!-- Opsi supplier akan dimasukkan melalui Ajax -->
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="harga">Harga</label>
-          <input type="number" class="form-control" id="harga" name="harga" placeholder="Masukkan Harga" />
-        </div>
-        <div class="form-group">
-          <button class="btn btn-primary" type="submit">Submit</button>
-        </div>
-      </form>
+    <div class="col">
+        <form action="" method="POST" id="formObat">
+            <div class="form-group">
+                <label for="kode">Kode Obat</label>
+                <input type="text" class="form-control" id="kode" name="kode" readonly />
+            </div>
+            <div class="form-group">
+                <label for="nama">Nama Obat</label>
+                <input type="text" class="form-control" id="nama" name="nama" />
+            </div>
+            <div class="form-group">
+                <label for="kode_kategori">Kode Kategori</label>
+                <input type="text" class="form-control" id="kode_kategori" name="kode_kategori" />
+            </div>
+            <div class="form-group">
+                <label for="kode_supplier">Kode Supplier</label>
+                <input type="text" class="form-control" id="kode_supplier" name="kode_supplier" />
+            </div>
+            <div class="form-group">
+                <label for="gambar">Gambar</label>
+                <input type="file" class="form-control" id="gambar" name="gambar" />
+            </div>
+            <div class="form-group">
+                <label for="harga">Harga</label>
+                <input type="number" class="form-control" id="harga" name="harga" />
+            </div>
+            <div class="form-group">
+                <button class="btn btn-danger" type="reset">Reset</button>
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </div>
+        </form>
     </div>
 </div>
--- admin/script/obat_input.js
+++ admin/script/obat_input.js
@@ -1,71 +1,57 @@
-$('#input').submit(function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    $.ajax({
-        type: 'POST',
-        url: host+"obat_input.php",
-        data: formData,
-        cache: false,
-        contentType: false, 
-        processData: false, 
-        dataType: 'json',
-        success: (result) => {
-            alert(result.msg);
-        },
-    });
-  })
+$(document).ready(function () {
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split("&"),
+            sParameterName,
+            i;
 
-   function populasiKategori() {
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split("=");
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+        return false;
+    };
+
+    var id = getUrlParameter("kode");
+
+    // menampilkan data by=id
     $.ajax({
         type: "GET",
-        url: host+"read_kategori.php", // Gantilah dengan URL API kategori
-        dataType: "json",
+        url: host + "/read_one_obat.php?id=" + id,
         async: true,
         cache: false,
         contentType: false,
         processData: false,
-        success: function (data) {
-            var categories = data.body.data;
-            var select = $("#kode_kategori");
+        dataType: "json",
+        success: function (response) {
+            var data = response.body.data[0];
 
-            // Kosongkan opsi pemilihan sebelum mengisi kembali
-            select.empty();
-
-            // Tambahkan opsi pemilihan untuk setiap kategori
-            for (var i = 0; i < categories.length; i++) {
-                select.append(`<option value="${categories[i].kode}">${categories[i].nama}</option>`);
-            }
+            $("#kode").val(data.kode);
+            $("#nama").val(data.nama);
+            $("#kode_kategori").val(data.kode_kategori);
+            $("#kode_supplier").val(data.kode_supplier);
+            $("#harga").val(data.harga);
         },
     });
-}
-// Panggil fungsi untuk mengisi opsi pemilihan kategori saat halaman dimuat
-$(document).ready(function () {
-    populasiKategori();
-});
-function populasiSuplier() {
-  $.ajax({
-      type: "GET",
-      url: host+"read_supplier.php", // Gantilah dengan URL API kategori
-      dataType: "json",
-      async: true,
-      cache: false,
-      contentType: false,
-      processData: false,
-      success: function (data) {
-          var categories = data.body.data;
-          var select = $("#kode_supplier");
 
-          // Kosongkan opsi pemilihan sebelum mengisi kembali
-          select.empty();
-
-          // Tambahkan opsi pemilihan untuk setiap kategori
-          for (var i = 0; i < categories.length; i++) {
-              select.append(`<option value="${categories[i].kode}">${categories[i].nama}</option>`);
-          }
-      },
-  });
-}
-// Panggil fungsi untuk mengisi opsi pemilihan kategori saat halaman dimuat
-$(document).ready(function () {
-  populasiSuplier();
+    // update data
+    $("#formObat").submit(function (e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        $.ajax({
+            type: "POST",
+            url: host + "update_obat.php",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            dataType: "json",
+            success: function (response) {
+                alert(response.msg);
+            },
+        });
+    });
 });
