72893ea06c885aec34a9ba606f6d93251f82bab7
MhdFikri44 <mhdfikr44@gmail.com>
2023-12-25 17:40:29 +0700

    24.bugfix

--- admin/supplier_edit.php
+++ admin/supplier_edit.php
@@ -1,12 +1,12 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3">
-    <h6><a href="?page=obat_data">Kembali</a></h6>
+    <h6><a href="?page=supplier_data">Kembali</a></h6>
 </div>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Edit Supplier</h1>
 </div>
 
 <div class="row">
-    <div class="col">
+    <div class="col-md-4">
         <form action="" method="POST" id="formSupplier">
             <div class="form-group">
                 <label for="kode">Kode</label>
