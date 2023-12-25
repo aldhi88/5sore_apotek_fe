aeba6f8def98a9b8a6a54a9ac1da55b89d9ddde3
Muhammad Fikri <130049564+MhdFikri44@users.noreply.github.com>
2023-12-25 21:10:39 +0700

    redirect

--- admin/script/supplier_edit.js
+++ admin/script/supplier_edit.js
@@ -50,7 +50,7 @@ $(document).ready(function () {
             dataType: "json",
             success: function (response) {
                 alert(response.msg);
-                location.href = host_fe + "?page=supplier_data";
+                location.href = host_fe + "admin/?page=supplier_data";
             },
         });
     });
