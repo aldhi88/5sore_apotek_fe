4f2174b521d61185399a1fe9aea6cbe9abc31e6a
MhdFikri44 <mhdfikr44@gmail.com>
2023-12-26 17:53:31 +0700

    bugfix

--- admin/script/obat_data.js
+++ admin/script/obat_data.js
@@ -1,62 +1,64 @@
 $(document).ready(function () {
+    readData();
     // api read
-    $.ajax({
-        type: "GET",
-        url: host + "read_obat.php",
-        dataType: "json",
-        async: true,
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: function (data) {
-            var dataObat = data.body.data;
-            for (var i = 0; i < dataObat.length; i++) {
-                $("#tableObat").append(
-                    `
+    function readData() {
+        $("#tableObat").empty();
+        $.ajax({
+            type: "GET",
+            url: host + "read_obat.php",
+            dataType: "json",
+            async: true,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                var dataObat = data.body.data;
+                for (var i = 0; i < dataObat.length; i++) {
+                    $("#tableObat").append(
+                        `
                     <tr>
                         <td>` +
-                        (i + 1) +
-                        `</td>
+                            (i + 1) +
+                            `</td>
                         <td>
                             <img src="` +
-                        host +
-                        dataObat[i].gambar +
-                        `" width="100" />
+                            host +
+                            dataObat[i].gambar +
+                            `" width="100" />
                         </td>
                         <td>` +
-                        dataObat[i].kode +
-                        `</td>
+                            dataObat[i].kode +
+                            `</td>
                         <td>` +
-                        dataObat[i].obat +
-                        `</td>
+                            dataObat[i].nama +
+                            `</td>
                         <td>` +
-                        dataObat[i].supplier +
-                        `</td>
+                            dataObat[i].kode_supplier +
+                            `</td>
                         <td>` +
-                        dataObat[i].kategori +
-                        `</td>
+                            dataObat[i].kode_kategori +
+                            `</td>
                         <td>` +
-                        dataObat[i].harga +
-                        `</td>
+                            dataObat[i].harga +
+                            `</td>
                         <td>
-                            <button id="hapus" class="btn btn-danger"
-                                <a class="text-light" href="?page=obat_edit&kode=` + +
-                        dataObat[i].kode +
-                        `">
-                            Hapus
-                            </button>
-                            <button id="edit" class="btn btn-primary">
+                        <button id="hapus" class="btn btn-danger" value="` +
+                            dataObat[i].kode +
+                            `">Hapus
+                        </button>
+                        <button id="edit" class="btn btn-primary">
                                 <a class="text-light" href="?page=obat_edit&id=` +
-                        dataObat[i].kode +
-                        `">Edit</a>
+                            dataObat[i].kode +
+                            `">Edit</a>
                             </button>
                         </td>
                     </tr>
                     `
-                );
-            }
-        },
-    });
+                    );
+                }
+            },
+        });
+    }
 
     // api hapus
     $(document).on("click", "#hapus", function () {
@@ -65,15 +67,13 @@ $(document).ready(function () {
         if (confirm("Yakin ingin hapus data dengan kode=" + kode + "?")) {
             $.ajax({
                 type: "POST",
-                url: host + "hapus_obat.php",
+                url: host + "delete_obat.php",
                 data: { kode: kode },
                 dataType: "json",
                 async: true,
-                cache: false,
-                contentType: false,
-                processData: false,
                 success: function (response) {
                     alert(response.msg);
+                    readData();
                 },
             });
         }
