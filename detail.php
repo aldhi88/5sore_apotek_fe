d7b6cf0854bccd6b8e9586f7e24e7c3abdf86cdd
Aldhi <aldhi.albadri@gmail.com>
2023-12-27 16:00:02 +0700

    fix

--- detail.php
+++ detail.php
@@ -1,1 +1,33 @@
-halaman detail
+<div class="container my-3">
+    <a class="text-dark" href="">Kembali</a>
+    <div class="row mt-2">
+        <div class="col-8">
+            <div class="left-side">
+                <div class="card">
+                    <div class="card-body p-5">
+                        <img src="assets/img/2.jpg" class="img-fluid" alt="" />
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione ullam quisquam nihil quis culpa ipsam quod accusantium voluptas dolor necessitatibus voluptate odio voluptates, minima adipisci a iste est, quos porro? Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit. Ducimus quaerat laborum nihil aspernatur labore totam dolorum consectetur molestias, illo omnis tempora numquam repellendus impedit amet non placeat? A, atque dolor?
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="right-side">
+                <div class="card">
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Nama &emsp;&emsp;: Obat Bodrek</li>
+                            <li class="list-group-item">Harga &emsp;&emsp;: Rp. 10.000,-/pack</li>
+                            <li class="list-group-item">Supplier &emsp;: Toko Obat</li>
+                            <li class="list-group-item">Kategori &emsp;: Obat Umum</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
