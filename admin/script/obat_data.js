7424dd4c97c8a1035af17afea7852d5e28cd6b56
Aldhi <aldhi.albadri@gmail.com>
2023-12-27 13:33:09 +0700

    fix bug

--- admin/script/obat_data.js
+++ admin/script/obat_data.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
                             `">Hapus
                         </button>
                         <button id="edit" class="btn btn-primary">
-                                <a class="text-light" href="?page=obat_edit&id=` +
+                                <a class="text-light" href="?page=obat_edit&ikoded=` +
                             dataObat[i].kode +
                             `">Edit</a>
                             </button>
