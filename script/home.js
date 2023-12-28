8e8115f60c228cfd4a8337a4de775fdd268866aa
GebyRezeki <gebydmi10@gmail.com>
2023-12-28 10:37:22 +0700

    fix lokasi img

--- script/home.js
+++ script/home.js
@@ -18,11 +18,11 @@ $(document).ready(function () {
                     $("#data-obat").append(`
                         <div class="col mb-3">
                         <div class="card" style="width: 16rem">
-                            <img src="assets/img/`+obat[i].gambar+`" class="card-img-top" alt="" style="object-fit:cover;height:170px"/>
+                            <img src="`+ host +`upload/`+ obat[i].gambar +`" class="card-img-top" alt="" style="object-fit:cover;height:170px"/>
                             <div class="card-body">
-                                <h5 class="card-title"><strong>`+obat[i].nama+`</strong></h5>
+                                <h5 class="card-title"><strong>`+ obat[i].nama +`</strong></h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="?page=detail&kode=`+obat[i].kode+`" class="btn btn-sm btn-dark">Detail</a>
+                                <a href="?page=detail&kode=`+ obat[i].kode +`" class="btn btn-sm btn-dark">Detail</a>
                             </div>
                         </div>
                         </div>
