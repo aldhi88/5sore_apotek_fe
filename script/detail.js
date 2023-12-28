0282f72c46efbff9415a05881999ca4c256e981b
zaky10062001 <zakytjr5@gmail.com>
2023-12-28 10:06:49 +0300

    url gambar

--- script/detail.js
+++ script/detail.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
             var data = response.body.data;
 
             $('#gambar-obat').append(`
-                <img src="assets/img/`+data.gambar+`" class="img-fluid" alt="" />
+                <img src="`+ host + 'upload/'+data.gambar+`" class="img-fluid" alt="" />
                 <p>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione ullam quisquam nihil quis culpa ipsam quod accusantium voluptas dolor necessitatibus voluptate odio voluptates, minima adipisci a iste est, quos porro? Lorem ipsum dolor sit amet consectetur
                     adipisicing elit. Ducimus quaerat laborum nihil aspernatur labore totam dolorum consectetur molestias, illo omnis tempora numquam repellendus impedit amet non placeat? A, atque dolor?
