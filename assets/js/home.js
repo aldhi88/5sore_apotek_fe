b762e6aefa58e207f498a14f685935ff3df52121
GebyRezeki <gebydmi10@gmail.com>
2023-12-28 07:29:06 +0700

    53. home fe

---
+++ assets/js/home.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    readData();
+
+    function readData() {
+        $("#tableObat").empty();
+        $.ajax({
+            type: "GET",
+            url: host + "read_obat.php",
+            dataType: "json",
+            async: true,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                var obat = data.body.data;
+                console.log(obat);
+                for (var i = 0; i < obat.length; i++) {
+                    $("#data-obat").append(`
+                        <div class="col mb-3">
+                        <div class="card" style="width: 16rem">
+                            <img src="assets/img/`+obat[i].gambar+`" class="card-img-top" alt="" style="object-fit:cover;height:170px"/>
+                            <div class="card-body">
+                                <h5 class="card-title"><strong>`+obat[i].nama+`</strong></h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="?page=detail&kode=`+obat[i].kode+`" class="btn btn-sm btn-dark">Detail</a>
+                            </div>
+                        </div>
+                        </div>
+                    `);
+                }
+            },
+        });
+    }
+});
--- index.php
+++ index.php
@@ -1,43 +1,50 @@
 <?php
-if(!isset($_REQUEST['page'])){
+if (!isset($_REQUEST['page'])) {
     header('location:?page=home');
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        <!-- bootstrap -->
-        <link rel="stylesheet" href="assets/lib/bootstrap-4.6.2-dist/css/bootstrap.min.css" />
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        <!-- css external -->
-        <link rel="stylesheet" href="assets/css/index.css" />
-        <title>Apotek | Home</title>
-    </head>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/lib/bootstrap-4.6.2-dist/css/bootstrap.min.css" />
 
-    <body>
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div class="container">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link text-white-50" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                        </ul>
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </div>
+    <!-- css external -->
+    <link rel="stylesheet" href="assets/css/index.css" />
+    <title>Apotek | Home</title>
+    <script src="env.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="container">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link text-white-50" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
-            </nav>
-        </header>
+            </div>
+        </nav>
+    </header>
 
-        <main>
-            <?php include $_REQUEST['page'].".php"; ?>
-        </main>
-    </body>
+    <main>
+        <?php include $_REQUEST['page'] . ".php"; ?>
+    </main>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="assets/js/<?= $_REQUEST['page'] . ".js" ?>"></script>
+</body>
+
 </html>
